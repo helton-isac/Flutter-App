04798a78d7bc4754b09faa7a959ebfe0676484b0
Helton Isac <helton.isac@gmail.com>
2020-06-30 11:43:38 -0300

    Creating a Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Flutter Demo",
       theme: ThemeData(primaryColor: Colors.red, primarySwatch: Colors.red),
-      home: Scaffold(
-        backgroundColor: Colors.limeAccent,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => print("Fab pressed"),
-        ),
-        appBar: AppBar(
-          title: Text("Flutter Try - Home Page"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "You've pushed this button these many times:",
-                style: TextStyle(fontSize: 18),
-              ),
-              Text(
-                "0",
-                style: Theme.of(context).textTheme.headline5,
-                textScaleFactor: 3,
-              ),
-            ],
-          ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.limeAccent,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => print("Fab pressed"),
+      ),
+      appBar: AppBar(
+        title: Text("Flutter Try - Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "You've pushed this button these many times:",
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              "0",
+              style: Theme.of(context).textTheme.headline5,
+              textScaleFactor: 3,
+            ),
+          ],
         ),
       ),
     );
