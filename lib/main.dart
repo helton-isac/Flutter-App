8a2be86386c921f86f36abfdab3741458f3833a2
Helton Isac <helton.isac@gmail.com>
2020-06-30 10:42:02 -0300

    AppBar and Actions

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(primaryColor: Colors.red),
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.limeAccent,
+        appBar: AppBar(
+          title: Text("Flutter Try - Home Page"),
+          leading: Icon(Icons.arrow_back),
+          actions: <Widget>[
+            Icon(Icons.add_circle),
+            Icon(Icons.remove_circle),
+          ],
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 "You've pushed this button these many times:",
-                style: TextStyle(
-                  fontSize: 18
-                ),
+                style: TextStyle(fontSize: 18),
               ),
               Text(
                 "0",
-                style: Theme.of(context).textTheme.headline4,
+                style: Theme.of(context).textTheme.headline5,
                 textScaleFactor: 3,
               ),
             ],
